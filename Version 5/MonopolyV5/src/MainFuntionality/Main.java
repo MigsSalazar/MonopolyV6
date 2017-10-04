fc460a9b13d06fc5908a5c2e991d5f8ffcb3fd22
MigsSalazar <Miguel@169.254.90.44>
2017-10-04 00:53:43 -0500

    adding gson

--- Version 5/MonopolyV5/src/MainFuntionality/Main.java
+++ Version 5/MonopolyV5/src/MainFuntionality/Main.java
@@ -1,5 +1,11 @@
 package MainFuntionality;
 
+import javax.swing.*;
+//import com.google.*;
+
 public class Main {
-	
+	public static void main(String[] args){
+		JOptionPane.showMessageDialog(null, "Welcome to ");
+		//Gson g = new Gson;
+	}
 }
