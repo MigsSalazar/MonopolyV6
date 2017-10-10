1e7edae7ca4eda88f3bb2c0f47ad3a5517e45358
Unknown <Unknown@Miguel-Salazar-Laptop>
2017-10-10 11:49:53 -0500

    Updating method attributes for Suite interface

--- Version 5/MonopolyV5/src/main/java/models/Suite.java
+++ Version 5/MonopolyV5/src/main/java/models/Suite.java
@@ -6,38 +6,38 @@ public interface Suite {
 	 * Returns the title of the suite as a String object
 	 * @return	String of the title: a color name
 	 */
-	String getColor();
+	public abstract String getColor();
 
 	/**
 	 * Runs through a player's properties to see if they hold the complete suite stored in this object
 	 * @param pl	Player to analyze
 	 * @return		a boolean confirming or denying the player's claim to hold a monopoly
 	 */
-	boolean playerHasMonopoly(Player pl);
+	public abstract boolean playerHasMonopoly(Player pl);
 
 	/**
 	 * Searches through the suite to determine the highest property value in the suite
 	 * @return	integer of the largest property value found
 	 */
-	int largestGrade();
+	public abstract int largestGrade();
 
 	/**
 	 * Searches through the suite to determine the lowest property value in the suite
 	 * @return	integer of the smallest property value found
 	 */
-	int smallestGrade();
+	public abstract int smallestGrade();
 
 	/**
 	 * Checks to see if any properties contained in the suite have been mortgaged
 	 * @return	true if mortgaged, false if not
 	 */
-	boolean hasMortgage();
+	public abstract boolean hasMortgage();
 
 	/**
 	 * Determines the range of the property value.
 	 * Range should never be greater than 1 or less than -1
 	 * @return	integer value of the disparity
 	 */
-	int gradeDisparity();
+	public abstract int gradeDisparity();
 
 }
