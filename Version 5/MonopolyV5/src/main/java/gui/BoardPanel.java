1193d430e09e0510c3c84c173c4634526d4fc791
Unknown <Unknown@Miguel-Salazar-Laptop>
2017-10-25 14:27:00 -0500

    Oh boy, im in a world of pain
    making boardpanel and now its time to figure out how to make Gson work
    for me

--- Version 5/MonopolyV5/src/main/java/gui/BoardPanel.java
+++ Version 5/MonopolyV5/src/main/java/gui/BoardPanel.java
@@ -6,12 +6,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 import main.java.models.Path;
 import main.java.models.PathStep;
-import main.java.models.Player;
+import main.java.models.Piece;
+
+import com.google.gson.*;
 
 /**
  * @author Miguel Salazar
@@ -24,24 +27,37 @@ public class BoardPanel extends JPanel {
 	 */
 	private static final long serialVersionUID = 1102084765976301993L;
 	
-	
-	private ArrayList<Path> playerPaths;
-	private HashMap<String, ImageIcon> images;
-	private ArrayList<ImageIcon> paintByNumber;
+	private ArrayList<Piece> gamePieces;
+	private ArrayList<Path> piecePaths;
+	private HashMap<String, ImageIcon> paintColors;
+	private String[][] paintByString;
 	private JLabel[][] tiles;
-	private static JLabel[][] defaultTiles;
+	private Border[] borders;
 	private GridLayout gid;
 	private File path;
+	private int width;
+	private int height;
 	
 	public BoardPanel(){
 		path = new File( System.getProperty("user.dir")+"/resources/image-sets/default-image-set/" );
-		
+		developBorders();
 	}
 	
 	public BoardPanel(File p){
 		
 	}
 	
+	public BoardPanel(Gson gfile){
+		
+	}
+	
+	private void developBorders(){
+		
+	}
+	
+	private void populateTiles(){
+		
+	}
 	
 	
 }
